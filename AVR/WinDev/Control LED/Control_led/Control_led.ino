7f7f96b08530b70642d37a66f9921493320e8ed7
Ala Hassine <contact.ala.hassine@gmail.com>
2025-09-28 16:04:19 +0100

    Control LED Via WinDev And MCU AVR

---
+++ AVR/WinDev/Control LED/Control_led/Control_led.ino
@@ -0,0 +1,21 @@
+
+const byte led = 13;
+char data;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    data = Serial.read();
+    switch (data) {
+      case 'A' : digitalWrite(led, HIGH); break;
+      case 'B' : digitalWrite(led, LOW); break;
+    }
+  }
+}
